f8a8e65ac2c4a0f255c0207b284470a7ba0638d9
Askold Astakhov <astahov@element-soft.com>
2023-06-26 16:27:14 +0300

    react-query

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,18 @@ import {Provider} from "react-redux";
 
 import {store} from "./store/store";
 import Navigation from "./router/Navigation";
+import queryClient from "./libs/queryClient";
+import {QueryClientProvider} from "react-query";
+import {ReactQueryDevtools} from "react-query/devtools";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Provider store={store}>
-        <Navigation />
-    </Provider>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <Provider store={store}>
+                <Navigation/>
+            </Provider>
+
+            <ReactQueryDevtools position="bottom-right" />
+        </QueryClientProvider>
+    </React.StrictMode>,
 )
